c9673dacdbeebeca00fe77c41cfaea09f69a6868
myreedbricks <reedbricks@gmail.com>
2022-07-20 00:55:53 +0100

    Vite setup fixed

--- client/vite.config.js
+++ client/vite.config.js
@@ -6,18 +6,21 @@ const port = process.env.PORT || 4000
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  proxy: {
-    '/api/v1': {
-      target: `https://localhost:${port}`,
-      changeOrigin: true,
-      secure: false,
-      ws: true,
-    },
-    '/api/v2': {
-      target: `https://localhost:${port}`,
-      changeOrigin: true,
-      secure: false,
-      ws: true,
+  host: true,
+  server: {
+    proxy: {
+      '/api/v1': {
+        target: `https://localhost:${port}`,
+        changeOrigin: true,
+        secure: false,
+        ws: true,
+      },
+      '/api/v2': {
+        target: `https://localhost:${port}`,
+        changeOrigin: true,
+        secure: false,
+        ws: true,
+      }
     }
   }
 })
