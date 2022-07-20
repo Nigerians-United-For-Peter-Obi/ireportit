c40bb4048e50a1ae10db643077ab8379d1e99812
myreedbricks <reedbricks@gmail.com>
2022-07-20 15:27:01 +0100

    Checking componebts

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,15 @@
 import { useEffect } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
+import Tublet from "./components/tubelet/Tublet"
+
 function App() {
   useEffect(() => {
     document.title = "We move"
   })
 
   return (
-    <><h1 className='text-success'>Welcome my guy</h1></>
+    <><Tublet /></>
   )
 }
 
