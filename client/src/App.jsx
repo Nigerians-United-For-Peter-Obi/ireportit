f1d645d789fccac52c353e36b90714ad1fd9d4a7
myreedbricks <reedbricks@gmail.com>
2022-07-20 02:33:25 +0100

    GUI fix

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,11 @@
+import { useEffect } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-
 function App() {
+  useEffect(() => {
+    document.title = "We move"
+  })
+
   return (
     <><h1 className='text-success'>Welcome my guy</h1></>
   )
