e1fc898f5b8e9b46739f85778ad3042c56a55c2f
myreedbricks <reedbricks@gmail.com>
2022-07-21 10:48:10 +0100

    GUI Design

--- client/src/assets/js/script.js
+++ client/src/assets/js/script.js
@@ -17,11 +17,13 @@ Design and Developed by: Gentechtree.com
 [ End table content ]
 ======================================
 */
-(function(jQuery) {
+(function (jQuery) {
     "use strict";
-    jQuery(window).on('load', function(e) {
+    jQuery(window).on('load', function (e) {
 
         jQuery('p:empty').remove();
+
+        alert(0);
 
         /*------------------------
                 Page Loader
@@ -31,7 +33,7 @@ Design and Developed by: Gentechtree.com
         /*------------------------
                 Search Button
         --------------------------*/
-        jQuery('#gen-seacrh-btn').on('click', function() {
+        jQuery('#gen-seacrh-btn').on('click', function () {
             jQuery('.gen-search-form').slideToggle();
             jQuery('.gen-search-form').toggleClass('gen-form-show');
             if (jQuery('.gen-search-form').hasClass("gen-form-show")) {
@@ -42,11 +44,11 @@ Design and Developed by: Gentechtree.com
         });
 
         jQuery('.gen-account-menu').hide();
-         jQuery('#gen-user-btn').on('click', function(e) {
-            
+        jQuery('#gen-user-btn').on('click', function (e) {
+
             jQuery('.gen-account-menu').slideToggle();
 
-             e.stopPropagation();
+            e.stopPropagation();
             // jQuery('.gen-account-menu').toggleClass('gen-form-show');
             // if (jQuery('.gen-account-menu').hasClass("gen-form-show")) {
             //     jQuery(this).html('<i class="fa fa-times"></i>');
@@ -55,36 +57,33 @@ Design and Developed by: Gentechtree.com
             // }
         });
 
-        jQuery('body').on('click' , function(){
-            if(jQuery('.gen-account-menu').is(":visible"))
-            {
+        jQuery('body').on('click', function () {
+            if (jQuery('.gen-account-menu').is(":visible")) {
                 jQuery('.gen-account-menu').slideUp();
             }
         });
         /*------------------------
                 Sidebar Toggle
         --------------------------*/
-        jQuery("#gen-toggle-btn").on('click', function() {
+        jQuery("#gen-toggle-btn").on('click', function () {
             jQuery('#gen-sidebar-menu-contain').toggleClass("active");
         });
-        jQuery('.gen-toggle-btn').click(function() {
+        jQuery('.gen-toggle-btn').click(function () {
             jQuery('body').addClass('gen-siderbar-open');
         });
-        jQuery('.gen-close').click(function() {
+        jQuery('.gen-close').click(function () {
             jQuery('body').removeClass('gen-siderbar-open');
         });
         /*------------------------
                 Sticky Header
         --------------------------*/
         var view_width = jQuery(window).width();
-        if (!jQuery('header').hasClass('gen-header-default') && view_width >= 1023)
-        {
+        if (!jQuery('header').hasClass('gen-header-default') && view_width >= 1023) {
             var height = jQuery('header').height();
             jQuery('.gen-breadcrumb').css('padding-top', height * 1.3);
         }
-        if (jQuery('header').hasClass('gen-header-default'))
-        {
-            jQuery(window).scroll(function() {
+        if (jQuery('header').hasClass('gen-header-default')) {
+            jQuery(window).scroll(function () {
                 var scrollTop = jQuery(window).scrollTop();
                 if (scrollTop > 300) {
                     jQuery('.gen-bottom-header').addClass('gen-header-sticky animated fadeInDown animate__faster');
@@ -94,7 +93,7 @@ Design and Developed by: Gentechtree.com
             });
         }
         if (jQuery('header').hasClass('gen-has-sticky')) {
-            jQuery(window).scroll(function() {
+            jQuery(window).scroll(function () {
                 var scrollTop = jQuery(window).scrollTop();
                 if (scrollTop > 300) {
                     jQuery('header').addClass('gen-header-sticky animated fadeInDown animate__faster');
@@ -107,14 +106,14 @@ Design and Developed by: Gentechtree.com
                 Back To Top
         --------------------------*/
         jQuery('#back-to-top').fadeOut();
-        jQuery(window).on("scroll", function() {
+        jQuery(window).on("scroll", function () {
             if (jQuery(this).scrollTop() > 250) {
                 jQuery('#back-to-top').fadeIn(1400);
             } else {
                 jQuery('#back-to-top').fadeOut(400);
             }
         });
-        jQuery('#top').on('click', function() {
+        jQuery('#top').on('click', function () {
             jQuery('top').tooltip('hide');
             jQuery('body,html').animate({
                 scrollTop: 0
@@ -122,16 +121,15 @@ Design and Developed by: Gentechtree.com
             return false;
         });
 
-        if(jQuery('.tv-show-back-data').length)
-        {
+        if (jQuery('.tv-show-back-data').length) {
             var url = jQuery('.tv-show-back-data').data('url');
             console.log(url);
             var html = '';
             html += `<div class="tv-single-background">
-                <img src="`+url+`">
+                <img src="`+ url + `">
             </div>`;
             jQuery('#main').prepend(html);
-           
+
         }
     });
 })(jQuery);
