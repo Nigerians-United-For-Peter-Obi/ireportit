9ef9cde07ffed302bd32da58e61a59832d33bb85
myreedbricks <reedbricks@gmail.com>
2022-07-21 02:46:39 +0100

    Checking componebts

---
+++ client/src/components/Loader.jsx
@@ -0,0 +1,14 @@
+
+function Loader() {
+    return (
+        <>
+            <div id="gen-loading">
+                <div id="gen-loading-center">
+                    <img src="images/logo-1.png" alt="loading" />
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default Loader
---
+++ client/src/components/layouts/Footer.jsx
@@ -0,0 +1,11 @@
+
+import React from "react";
+
+function Header(params) {
+
+    return (
+        <></>
+    )
+}
+
+export default Header
