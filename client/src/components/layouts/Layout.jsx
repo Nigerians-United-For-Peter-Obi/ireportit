a750283425ba4825f6669c74115128ed38f2da29
myreedbricks <reedbricks@gmail.com>
2022-07-21 10:56:30 +0100

    GUI Design

--- client/src/components/layouts/Layout.jsx
+++ client/src/components/layouts/Layout.jsx
@@ -7,19 +7,19 @@ const Layout = ({ children }) => {
     return (
         <React.Fragment>
             <Helmet>
-                <script type="script" src="../../src/assets/js/jquery-3.6.0.min.js"></script>
-                <script type="script" src="../../src/assets/js/asyncloader.min.js"></script>
-                <script type="script" src="../../src/assets/js/bootstrap.min.js"></script>
-                <script type="script" src="../../src/assets/js/owl.carousel.min.js"></script>
-                <script type="script" src="../../src/assets/js/jquery.waypoints.min.js"></script>
-                <script type="script" src="../../src/assets/js/jquery.counterup.min.js"></script>
-                <script type="script" src="../../src/assets/js/popper.min.js"></script>
-                <script type="script" src="../../src/assets/js/swiper-bundle.min.js"></script>
-                <script type="script" src="../../src/assets/js/isotope.pkgd.min.js"></script>
-                <script type="script" src="../../src/assets/js/jquery.magnific-popup.min.js"></script>
-                <script type="script" src="../../src/assets/js/slick.min.js"></script>
-                <script type="script" src="../../src/assets/js/streamlab-core.js"></script>
-                <script type="script" src="../../src/assets/js/script.js"></script>
+                <script type="script" src="/assets/js/jquery-3.6.0.min.js"></script>
+                <script type="script" src="/assets/js/asyncloader.min.js"></script>
+                <script type="script" src="/assets/js/bootstrap.min.js"></script>
+                <script type="script" src="/assets/js/owl.carousel.min.js"></script>
+                <script type="script" src="/assets/js/jquery.waypoints.min.js"></script>
+                <script type="script" src="/assets/js/jquery.counterup.min.js"></script>
+                <script type="script" src="/assets/js/popper.min.js"></script>
+                <script type="script" src="/assets/js/swiper-bundle.min.js"></script>
+                <script type="script" src="/assets/js/isotope.pkgd.min.js"></script>
+                <script type="script" src="/assets/js/jquery.magnific-popup.min.js"></script>
+                <script type="script" src="/assets/js/slick.min.js"></script>
+                <script type="script" src="/assets/js/streamlab-core.js"></script>
+                <script type="script" src="/assets/js/script.js"></script>
             </Helmet>
             <Header />
             {children}
